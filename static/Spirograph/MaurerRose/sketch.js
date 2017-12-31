7e6a0f54aa72bb9bebf5fb0364f6ce1a6addf2e4
subeshb1 <subeshb1@gmail.com>
2017-12-31 23:26:23 +0545

    vs3:

--- static/Spirograph/MaurerRose/sketch.js
+++ static/Spirograph/MaurerRose/sketch.js
@@ -1,77 +1,135 @@
-let k ;
+let k;
 let d = 8;
 let n = 5;
-let z = 100;
-let sliderD ;
-let sliderN ;
+let z = 10;
+let sliderD;
+let sliderN;
 let sliderA;
 let sliderZ;
+
 function setup() {
   // put setup code here
-  let can = createCanvas(1400,1400);
+  let can = createCanvas(1400, 1400);
   can.parent('parent');
-  sliderD = createSlider(1,359,97,1);
-  sliderN = createSlider(1,359,5,1);
-  sliderA = createSlider(1,360,360,1);
-  sliderZ = createSlider(0.1,1,0.1,0.1);
-  sliderD.touchMoved( ()=> redraw());
-  sliderN.touchMoved( ()=> redraw());
-  sliderA.touchMoved( ()=> redraw());
-  sliderZ.touchMoved( ()=> redraw());
+  sliderD = createSlider(1, 359, 90, 1);
+  sliderN = createSlider(1, 359, 2, 1);
+  sliderA = createSlider(1, 360, 360, 1);
+  sliderZ = createSlider(0.1, 1, 0.1, 0.1);
+  sliderD.touchMoved(() => redraw());
+  sliderN.touchMoved(() => redraw());
+  sliderA.touchMoved(() => redraw());
+  sliderZ.touchMoved(() => redraw());
   noLoop();
 }
 
 function draw() {
   // put drawing code here
-  angleMode(DEGREES);
+   angleMode(RADIANS);
   d = sliderD.value();
   n = sliderN.value();
-  z = lerp(100,1000,sliderZ.value());
+  z = lerp(100, 1000, sliderZ.value());
   let k = sliderA.value();
-  console.log(k);
   
+
   background(255);
-  translate(500,500);
+  translate(500, 500);
   push();
   noFill();
-  stroke(255);
+  
   strokeWeight(1.5);
   // beginShape();
-    let theta = 0;
-    let old = {x:0,y:0};
-    do {
-      
-      theta += d;
-      if(theta>=k) {
-        theta %= k;
-      }
-      let aux = (n*theta);
-      aux %= k;
-      angleMode(RADIANS); 
-      let x = aux*PI/180;
-      
-      let r = sin(x);
-      let t = (theta*Math.PI)/180; 
+  let T = 0,c = 0;
 
-      let pt =  toRectangular(r,t);
-     
-      let color = random(0,100);
-      stroke(color);
-      line(old.x,old.y,pt.x,pt.y);
-      old = pt;
-      // vertex(pt.x,pt.y);
-      angleMode(DEGREES);
-    }while(theta!=0);
   
+  do {
+    console.log("FIRST");
+    
+    let theta = T;
+    let p = {
+      x: (TWO_PI * theta) / 360,
+      y: sin( (TWO_PI * n * theta) / 360)
+    };
+    
+    
+    let old = toRectangular(p.x, p.y);
+
+    do {
+      console.log("SECOND");
+      theta += d;
+      if (theta >= 360) {
+        theta %= 360;
+      }
+      let aux = n * theta;
+      aux %= 360;
+
+      
+      let x = (aux * PI) / 180;
+      let r = sin(x);
+      let t = (theta*Math.PI)/180;
+
+      let pt = toRectangular(r, t);
+      stroke(random(0,100));
+      line(old.x, old.y, pt.x, pt.y);
+
+      c++;
+      console.log(theta);
+      console.log("END SECOND");
+      if(theta  === T)
+        break;
+        old  = pt;
+      
+      
+      
+    } while (1);
+    
+    if(c >= 360)
+      break;
+    T++;
+    console.log("END FIRST");
+  } while (1);
+
+  console.log(T,c);
   
+
   // endShape();
   pop();
 }
 
 
-function toRectangular(r,t) {
-  let pt= {};
-  pt.x = cos(t) * r *z;
-  pt.y = sin(t) * r *z;
+function toRectangular(r, t) {
+ 
+  let pt = {};
+  pt.x = cos(t) * r * z;
+  pt.y = sin(t) * r * z;
+ 
   return pt;
 }
+
+
+
+
+// let theta = 0;
+// let old = {x:0,y:0};
+// do {
+
+//   theta += d;
+//   if(theta>=k) {
+//     theta %= k;
+//   }
+//   let aux = (n*theta);
+//   aux %= k;
+//   angleMode(RADIANS); 
+//   let x = aux*PI/180;
+
+//   let r = sin(x);
+//   let t = (theta*Math.PI)/180; 
+
+//   let pt =  toRectangular(r,t);
+
+//   let color = random(0,100);
+//   stroke(color);
+//   line(old.x,old.y,pt.x,pt.y);
+//   old = pt;
+//   // vertex(pt.x,pt.y);
+//   angleMode(DEGREES);
+// }while(theta!=0);
--- static/Spirograph/Roses/sketch.js
+++ static/Spirograph/Roses/sketch.js
@@ -1,45 +1,226 @@
-let k ;
+class MaurerRoseV1 {
+  constructor() {
+    this.line = [];
+    this.lineCounter = 0;
+  }
+  calc() {
+    d = sliderD.value();
+    n = sliderN.value();
+    z = lerp(100, 1000, sliderZ.value());
+    let k = sliderA.value();
+    this.line = [];
+    let theta = 0;
+    let old = {
+      x: 0,
+      y: 0
+    };
+    do {
+
+      theta += d;
+      if (theta >= 360) {
+        theta %= 360;
+      }
+      let aux = (n * theta);
+      aux %= 360;
+      // angleMode(RADIANS);
+      let x = aux * PI / 180;
+
+      let r = Math.sin(x);
+      let t = (theta * Math.PI) / 180;
+
+      let pt = toRectangular(r, t);
+
+      this.line.push([old, pt]);
+      old = pt;
+
+    } while (theta != 0);
+
+
+
+
+  }
+
+
+  draw() {
+
+    stroke(1);
+    let val = lerp(0, this.line.length, sliderA.value());
+
+
+    for (let i = 0; i < val; i++) {
+      let line1 = this.line[i];
+
+      line(line1[0].x, line1[0].y, line1[1].x, line1[1].y);
+    }
+  }
+}
+
+
+
+class MaurerRoseV3 {
+  constructor() {
+    this.line = [];
+  }
+
+  calc() {
+    d = sliderD.value();
+    n = sliderN.value();
+     z = lerp(100, 1000, sliderZ.value());
+    let z1 = 360;
+    let k = sliderA.value();
+    this.line = [];
+
+    let T = 0,
+      c = 0;
+    do {
+      let theta = T;
+      let p = {
+        x: Math.PI*2 * theta / z1,
+        y: Math.sin(Math.PI*2 * n * theta / z1)
+      }
+      let old = toRectangular(p.x, p.y);
+      do {
+        theta += d;
+        if (theta >= z1)
+          theta %= z1;
+        let aux = n * theta;
+        aux %= z1;
+       
+        
+        let x = aux * Math.PI*2 / z1;
+        
+        let r = Math.sin(x);
+        let t = Math.PI*2 * theta / z1;
+        // console.log(t);
+        let pt = toRectangular(r, t);
+      console.log(pt);
+      
+        
+        
+        this.line.push([old, pt]);
+        
+        if (theta == T)
+        break;
+        old = pt;
+        c++;
+      } while (1);
+      if (c >= z1)
+        break;
+      T++;
+
+    } while (1);
+
+  }
+
+  draw() {
+    stroke(1);
+    let val = lerp(0, this.line.length, sliderA.value());
+
+
+    for (let i = 0; i < val; i++) {
+      let line1 = this.line[i];
+      // console.log(line1);
+      
+      stroke(random(0,100));
+      line(line1[0].x, line1[0].y, line1[1].x, line1[1].y);
+    }
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let k;
 let d = 8;
 let n = 5;
-let sliderD ;
-let sliderN ;
+let z = 10;
+let sliderD;
+let sliderN;
 let sliderA;
+let sliderZ;
+let subesh = new MaurerRoseV3();
+
 function setup() {
   // put setup code here
-  let can = createCanvas(600,400);
-  sliderD = createSlider(1,9,4);
-  sliderN = createSlider(1,9,4);
-  sliderA = createSlider(0,360,360,1);
-  sliderD.touchMoved( ()=> redraw());
-  sliderN.touchMoved( ()=> redraw());
-  sliderA.touchMoved( ()=> redraw());
+  let can = createCanvas(1400, 1400);
+  can.parent('parent');
+  sliderD = createSlider(1, 359, 72, 1);
+  sliderN = createSlider(1, 359, 6, 1);
+  sliderA = createSlider(0, 1, 1, 0.01);
+  sliderZ = createSlider(0.1, 1, 0.1, 0.1);
+  sliderD.touchMoved(() => {
+    subesh.calc();
+    redraw();
+  });
+  sliderN.touchMoved(() => {
+    subesh.calc();
+    redraw();
+  });
+  sliderA.touchMoved(() => {
+    redraw();
+  });
+  sliderZ.touchMoved(() => {
+    subesh.calc();
+    redraw();
+  });
+  subesh.calc();
   noLoop();
 }
 
 function draw() {
-  angleMode(DEGREES);
   // put drawing code here
-  d = sliderD.value();
-  n = sliderN.value();
-  let k = n / d;
-  let max =sliderA.value();
-  
-  
-  background(0);
-  translate(width/2,height/2);
+  background(255);
+  translate(500, 500);
+
   push();
-  beginShape();
-  
   noFill();
-  strokeWeight(1);
-  for(let a = 0; a  <  max  *  d ; a += 1) {
-    let r = 200*cos(k*a);
-    let x = r * cos(a);
-    let y = r * sin(a);
-    stroke(random(50,255),random(50,255),random(50,255));  
-    vertex(x,y);
-    stroke(random(50,255),random(50,255),random(50,255));  
-  }
-  endShape();
+
+  strokeWeight(1.5);
+  subesh.draw();
   pop();
+
+}
+
+
+function toRectangular(r, t) {
+
+  let pt = {};
+  pt.x = Math.cos(t) * r * z;
+  pt.y = Math.sin(t) * r * z;
+
+  return pt;
 }
